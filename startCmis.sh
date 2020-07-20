61c6311471a9b7b8c8d6fd5ed318ea29d9c0566c
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-07-20 09:49:19 -0700

    Start CMIS

---
+++ startCmis.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set properties.path=/etc/bonita/CoreModel.properties
+export properties.path
+java -jar CMISCall-1.0.0-jar-with-dependencies.jar http://ledcb970.frmon.danet:8080/ meirami bpm '' 6779299849492924173 working
