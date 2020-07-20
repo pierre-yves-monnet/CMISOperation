30f7838fee00ed8dcd78acbe153f7af5b98c1afc
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2020-07-20 10:27:20 -0700

    Add the classpath

--- startCmis.sh
+++ startCmis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-set properties.path=/etc/bonita/CoreModel.properties
+properties.path=/etc/bonita/CoreModel.properties
 export properties.path
-java -jar CMISCall-1.1.0-jar-with-dependencies.jar http://ledcb970.frmon.danet:8080/ meirami bpm '' 6779299849492924173 working /etc/bonita/CoreModel.properties
+java  -classpath . -jar CMISCall-1.1.0-jar-with-dependencies.jar http://ledcb970.frmon.danet:8080/ meirami bpm '' 6779299849492924173 working /etc/bonita/CoreModel.properties
